e843e16493f2b8afadf57d5a979277ae001fcc4b
Hybrid <dkshybrid@gmail.com>
2026-08-10 20:07:17 -0400

    prepare v0.1.8-hotfix.1 release

--- internal/deckbuilder/app/startup.go
+++ internal/deckbuilder/app/startup.go
@@ -28,7 +28,7 @@ import (
 const (
 	applicationID      = localdata.SharedApplicationID
 	applicationName    = "Caster's Compendium"
-	applicationVersion = "0.1.8"
+	applicationVersion = "0.1.8-hotfix.1"
 )
 
 const setupDownloadWorkers = 6
